4ee3b92a56bf68522b2013520875e7d388117e52
Thomas Vandekerckhove <totovdk@gmail.com>
2024-08-20 16:04:23 +0000

    Saving files  + deleting old team stats

--- models/intermediate/ranking_shooting_guard.sql
+++ models/intermediate/ranking_shooting_guard.sql
@@ -6,5 +6,5 @@ tm,
 SG_global_stats
 ,RANK() OVER (ORDER BY SG_global_stats DESC ) AS ranking_SG
 FROM {{ ref('player_per_36_min_played_advanced_stats') }}
-WHERE pos LIKE 'SG'
+WHERE pos LIKE 'SG%' OR pos LIKE '%SG'
 ORDER BY SG_global_stats DESC
